5013330e830c48a879e65c347f8c39bf02a64d91
Ashwini <Ashwini@yahoo.com>
2022-07-25 23:04:06 +0530

    Added Login API Test

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -13,6 +13,9 @@ import io.restassured.http.Header;
 
 public class LoginAPITest {
 	//
+
+	private String token;
+
 	@BeforeMethod(description = "intializing  the baseURI for api testing")
 	public void setup() {
 		baseURI = "http://139.59.91.96:9000";
@@ -23,9 +26,16 @@ public class LoginAPITest {
 			"regression" }, priority = 1)
 	public void verifyLoginAPI() {
 
-		given().when().header(new Header("content-type", "application/json")).and()
+		token = given().when().header(new Header("content-type", "application/json")).and()
 				.body(new LoginPOJO("iamfd", "password").toJson()).and().post("v1/login").then().log().all().and()
-				.assertThat().statusCode(200).and().assertThat().body(Matchers.containsString("Success"));
+				.assertThat().statusCode(200).and().assertThat().body(Matchers.containsString("Success")).and()
+				.extract().jsonPath().getString("data.token");
+		System.out.println("-------------" + token);
+	}
+
+	@Test(description = "verify if create job API request is working or not", groups = { "sanity", "smoke", "e2e",
+			"api", "regression" }, priority = 2)
+	public void createJobAPITest() {
 
 	}
 
